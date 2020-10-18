6d542f250052dcd98f8a8fd964e9651c1b5c9d6a
Shivam Yadav <shivam.yadav.09087@gmail.com>
2020-10-18 19:55:08 +0530

    Revert "changes to weather app layout"
    This reverts commit 7688c6066175cb27b1126b330146f836de224f31.

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.weatherapp;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,6 +26,7 @@ import java.net.URLEncoder;
 public class MainActivity extends AppCompatActivity {
 
     EditText cityName;
+    TextView resultTextView;
 
     public void findWeather(View view){
         Log.i("cityName", cityName.getText().toString());
@@ -36,21 +35,16 @@ public class MainActivity extends AppCompatActivity {
         InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         mgr.hideSoftInputFromWindow(cityName.getWindowToken(), 0);
 
-        String API_KEY = "d70bdc756aae9a72b2f9024803584277";
-        //String API_KEY = "Enter your API key";
-        if(API_KEY == "Enter your API key"){
-            Toast.makeText(getApplicationContext(), "API key not set", Toast.LENGTH_LONG).show();
+        String API_KEY = "Enter your API Key";
+        try {
+            String encodedCityName = URLEncoder.encode(cityName.getText().toString(), "UTF-8"); //encoding for cities with spaces, apostrophes
+            DownloadTask task = new DownloadTask();
+            task.execute("http://api.openweathermap.org/data/2.5/weather?q="+encodedCityName+"&appid="+API_KEY);
+            //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
         }
-        else {
-            try {
-                String encodedCityName = URLEncoder.encode(cityName.getText().toString(), "UTF-8"); //encoding for cities with spaces, apostrophes
-                DownloadTask task = new DownloadTask();
-                task.execute("http://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName + "&appid=" + API_KEY);
-                //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
-            }
+        catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
         }
 
     }
@@ -61,13 +55,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cityName = findViewById(R.id.id_userText);
+        resultTextView = findViewById(R.id.id_resultTextView);
     }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... urls) {
-            StringBuilder result = new StringBuilder();
+            String result = "";
             URL url;
             HttpURLConnection urlConnection = null;
             try {
@@ -78,12 +73,12 @@ public class MainActivity extends AppCompatActivity {
                 int data = reader.read();
                 while (data != -1) {
                     char current = (char) data;
-                    result.append(current);
+                    result += current;
                     data = reader.read();
                 }
-                return result.toString();
+                return result;
             } catch (Exception e) {
-                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
             }
             return null;
         }
@@ -97,16 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 String message = "";
                 JSONObject jsonObject = new JSONObject(result);
                 String weatherInfo = jsonObject.getString("weather");
-                /* --------Getting temperature, pressure and humidity-------------
-                -----------Put all the data to display in an array and pass the array to the intent-------------
-                String temp_press_humid = jsonObject.getString("main");
-                Log.i("main", temp_press_humid);
-                JSONObject arrTPH = new JSONObject(temp_press_humid);
-                String temp = arrTPH.getString("temp");
-                String press = arrTPH.getString("pressure");
-                String hum = arrTPH.getString("humidity");
-                Log.i("temp",press);
-                */
                 Log.i("Weather content", weatherInfo);
                 JSONArray arr = new JSONArray(weatherInfo);
                 for (int i = 0; i < arr.length(); i++) {
@@ -120,16 +105,13 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 if (message != "") {
-                    //resultTextView.setText(message);
-                    Intent intent = new Intent(MainActivity.this, com.example.weatherapp.Pop.class);
-                    intent.putExtra("message", message);
-                    startActivity(intent);
+                    resultTextView.setText(message);
                 } else {
-                    Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
                 }
             } catch (JSONException e) {
 
-                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
 
             }
 
